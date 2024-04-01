5ecc17fea30ba00796a330ebf052e2fc697c6758
Matthew Steinhardt <Matthew.steinhardt@gmail.com>
2024-04-01 11:33:20 -0700

    Google API
    - Added authentication

--- src/main.js
+++ src/main.js
@@ -5,29 +5,124 @@
 //      GOOGLE_CLIENT_ID = <project client id>
 //      GOOGLE_PROJECT_ID = <project id>
 //      GOOGLE_SECRET = <client secret>
-require('dotenv').config();
+
+require("dotenv").config();
 // File System
-const fs = require('fs');
+const fs = require("fs");
+const path = require("path");
+const process = require("process");
 // Discord
-const {Client, Events, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions, SlashCommandBuilder, ButtonStyle, InteractionType} = require('discord.js');
-const DiscordClient = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]});
+const {
+  Client,
+  Events,
+  GatewayIntentBits,
+  EmbedBuilder,
+  PermissionsBitField,
+  Permissions,
+  SlashCommandBuilder,
+  ButtonStyle,
+  InteractionType,
+} = require("discord.js");
+const DiscordClient = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+// -- Load Google Credentials -- //
+const GoogleCreds = require("./config").Sheets.Credentials;
+const { authenticate } = require("@google-cloud/local-auth");
+const { google } = require("googleapis");
+// If modifying these scopes, delete token.json.
+const SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = path.join(process.cwd(), "token.json");
+const CREDENTIALS_PATH = path.join(process.cwd(), "credentials.json");
+
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * @return {Promise<OAuth2Client|null>}
+ */
+async function googleLoadSavedCredentialsIfExist() {
+  try {
+    return google.auth.fromJSON(JSON.stringify(GoogleCreds));
+  } catch (err) {
+    return null;
+  }
+}
+
+/**
+ * Serializes credentials to a file compatible with GoogleAuth.fromJSON.
+ *
+ * @param {OAuth2Client} client
+ * @return {Promise<void>}
+ */
+async function googleSaveCredentials(client) {
+  const key = GoogleCreds.installed || GoogleCreds.web;
+  const payload = JSON.stringify({
+    type: "authorized_user",
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFileSync(TOKEN_PATH, payload, () => {});
+}
+
+/**
+ * Writes out a json file needed for google authentication. 
+ * NOTE: This file should be IGNORED by git!
+ */
+function writeCredentialsFile() {
+    const payload = JSON.stringify(GoogleCreds);
+    fs.writeFileSync(CREDENTIALS_PATH, payload, () => {});
+    return CREDENTIALS_PATH
+}
+
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function googleAuthorize() {
+  let client = await googleLoadSavedCredentialsIfExist();
+  writeCredentialsFile()
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await googleSaveCredentials(client);
+  }
+  return client;
+}
+const GoogleClient = googleAuthorize();
 
 // -- Initialization -- //
-function Init()
-{
-    // Link bot functions to DiscordClient object
-    const BotFunctions = fs.readdirSync("./src/functions").filter(file => file.endsWith(".js"));
-    for (file of BotFunctions) {
-        require(`./functions/${file}`)(DiscordClient);
-    }
+function Init() {
+  // Link bot functions to DiscordClient object
+  const BotFunctions = fs
+    .readdirSync("./src/functions")
+    .filter((file) => file.endsWith(".js"));
+  for (file of BotFunctions) {
+    require(`./functions/${file}`)(DiscordClient);
+  }
 
-    // Link bot events to DiscordClient object
-    const BotEvents = fs.readdirSync("./src/events").filter(file => file.endsWith(".js"));
-    DiscordClient.handleEvents(BotEvents, "./src/events");
+  // Link bot events to DiscordClient object
+  const BotEvents = fs
+    .readdirSync("./src/events")
+    .filter((file) => file.endsWith(".js"));
+  DiscordClient.handleEvents(BotEvents, "./src/events");
 
-    // Login to Discord
-    DiscordClient.login(process.env.DISCORD_TOKEN);
+  // Login to Discord
+  DiscordClient.login(process.env.DISCORD_TOKEN);
 }
 
 // Make sure to run Initialization or else the bot won't work!
-Init()
+Init();
